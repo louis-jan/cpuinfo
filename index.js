5c4feebfc1763468f707cf4d044df6349ee3897f
Louis <louis@jan.ai>
2024-07-03 22:35:15 +0700

    add cpuinfo and node-pre-gyp

---
+++ index.js
@@ -0,0 +1,9 @@
+var binary = require("node-pre-gyp");
+var path = require("path");
+var binding_path = binary.find(
+  path.resolve(path.join(__dirname, "./package.json"))
+);
+var binding = require(binding_path);
+module.exports = exports = { cpuInfo: binding };
+
+// console.log(binding.cpuInfo());
